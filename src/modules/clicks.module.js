af46f027f0caf3185a1ed26ff613461f0b978f3d
dopa <ISPQ@bk.ru>
2023-11-05 20:30:33 +0200

    formatting

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -3,28 +3,28 @@ import { Module } from "../core/module"
 export class ClicksModule extends Module {
   timer = 0
   // timer = prompt("Таймер в секундах")
-  count = 0;
-  result = 0;
+  count = 0
+  result = 0
   counting = () => {
     this.count++
     console.log("count", this.count)
-  };
+  }
   trigger() {
-    this.timer = prompt("Таймер в секундах");
+    this.timer = prompt("Таймер в секундах")
     window.addEventListener("mousedown", () => {
-      this.count++;
+      this.count++
       console.log("count", this.count) // нужен только для проверки
     })
     // setInterval(() => {
     //   if (this.timer > 0) {
-    //     console.log(`Таймер = ${(this.timer -= 1)} `);
+    //     console.log(`Таймер = ${(this.timer -= 1)} `)
     //   }
     // }, 1000)
     setTimeout(() => {
-      this.result = this.count;
+      this.result = this.count
       window.removeEventListener("mousedown", () => {
-        this.count++;
-        // console.log("count", this.count); // нужен только для проверки
+        this.count++
+        // console.log("count", this.count) // нужен только для проверки
       })
       alert(`Количество кликов = ${this.result}`)
     }, `${this.timer * 1000}`)
--- src/modules/custom-message.module.js
+++ src/modules/custom-message.module.js
@@ -1,11 +1,11 @@
-import {random} from '../utils';
+import {random} from '../utils'
 import {Module} from '../core/module'
 
 export class CustomMessage extends Module {
 
   constructor(type, text) {
 
-    super(type, text);
+    super(type, text)
     this.customMessage = [
       {
         text: "Велики те, кто видит, что миром правят мысли..",
@@ -13,7 +13,7 @@ export class CustomMessage extends Module {
         id: 1,
       },
       {
-        text: "Если вы думаете, что на что-то способны, вы правы; если думаете, что у вас ничего не получится - вы тоже правы.",
+        text: "Если вы думаете, что на что-то способны, вы правы если думаете, что у вас ничего не получится - вы тоже правы.",
         author: "Генри Форд",
         id: 2,
       },
@@ -22,38 +22,38 @@ export class CustomMessage extends Module {
         author: "Уоррен Баффет",
         id: 3,
       },
-    ];
+    ]
   }
   
   trigger() {
     
-    const randomNumber = random(0, this.customMessage.length-1);
-    const mainContainer = document.querySelector('.main-container');
+    const randomNumber = random(0, this.customMessage.length-1)
+    const mainContainer = document.querySelector('.main-container')
     
-    const quote = document.createElement('blockquote');
-    quote.className = 'quote';
-    quote.id = this.customMessage[randomNumber].id;
+    const quote = document.createElement('blockquote')
+    quote.className = 'quote'
+    quote.id = this.customMessage[randomNumber].id
     
-    const customText = document.createElement('p');
-    customText.textContent = this.customMessage[randomNumber].text;
+    const customText = document.createElement('p')
+    customText.textContent = this.customMessage[randomNumber].text
 
-    const autorForText = document.createElement('cite');
-    autorForText.textContent = this.customMessage[randomNumber].author;
+    const autorForText = document.createElement('cite')
+    autorForText.textContent = this.customMessage[randomNumber].author
     
     quote.append(customText, autorForText)
-    mainContainer.append(quote);
+    mainContainer.append(quote)
     
     setTimeout(() => {
-      quote.classList.add('open');
-    }, 0);
+      quote.classList.add('open')
+    }, 0)
 
     setTimeout(() => {
-      quote.classList.remove('open');
-    }, 3000);
+      quote.classList.remove('open')
+    }, 3000)
 
     setTimeout(() => {
-      quote.remove();
-    }, 4000);
+      quote.remove()
+    }, 4000)
     
   }
 
