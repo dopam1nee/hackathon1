92311fdd3f8c8b9c45c96e02f69d91a6afc0662f
dopa <ISPQ@bk.ru>
2023-11-05 20:37:34 +0200

    formatting

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,19 +1,19 @@
-import { Module } from "../core/module"
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
   timer = 0
-  // timer = prompt("Таймер в секундах")
+  // timer = prompt('Таймер в секундах')
   count = 0
   result = 0
   counting = () => {
     this.count++
-    console.log("count", this.count)
+    console.log('count', this.count)
   }
   trigger() {
-    this.timer = prompt("Таймер в секундах")
-    window.addEventListener("mousedown", () => {
+    this.timer = prompt('Таймер в секундах')
+    window.addEventListener('mousedown', () => {
       this.count++
-      console.log("count", this.count) // нужен только для проверки
+      console.log('count', this.count) // нужен только для проверки
     })
     // setInterval(() => {
     //   if (this.timer > 0) {
@@ -22,9 +22,9 @@ export class ClicksModule extends Module {
     // }, 1000)
     setTimeout(() => {
       this.result = this.count
-      window.removeEventListener("mousedown", () => {
+      window.removeEventListener('mousedown', () => {
         this.count++
-        // console.log("count", this.count) // нужен только для проверки
+        // console.log('count', this.count) // нужен только для проверки
       })
       alert(`Количество кликов = ${this.result}`)
     }, `${this.timer * 1000}`)
